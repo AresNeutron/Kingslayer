aa42b3732a328291310c1bd9ce45e40c60976eb5
AresNeutron <arexneutron@gmail.com>
2025-07-28 16:32:07 -0300

    migrating client from next js to react vite

---
+++ gui/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold underline text-blue-600">
+        ¡Hola, Tailwind CSS funciona!
+      </h1>
+      <p className="mt-4 p-2 bg-green-200 rounded-md">
+        Esta es una prueba con clases de Tailwind.
+      </p>
+    </>
+  )
+}
+
+export default App
